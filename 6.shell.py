69b7fef239582430640e8af25a4aea11c2be5883
DaoHaiLong <daohailong.06122000@gmail.com>
2021-04-28 08:27:32 +0700

    updatePracticalWork

--- 6.shell.py
+++ 6.shell.py
@@ -2,23 +2,27 @@ import os
 import subprocess
 def main():
     print(" Current Working Directory:" , os.getcwd())
-    # chamge directory at Home
-    os.chdir(os.getenv("HOME"))
+    # change directory at Home
+    Currentfolder=os.getenv("HOME")
+    cmd= os.chdir(Currentfolder)
     cmd=""
     while True:
-        cmd=input("DaoHaiLong@Acer:/> ")
+        cmd=input("DaoHaiLong@Acer:~$ ")
         if cmd.split()[0] == "cd":
             try:
                 #change directory from Home to the required folder
                 os.chdir(cmd[3:]) 
+                # display directory
+                print(" Current Working Directory:" , os.getcwd())   
             except FileNotFoundError:
                 # IF not found folder print:
                  print("bash: cd: No such file or directory")
-                 subprocess.run(cmd,check=True)            
+                 subprocess.run(cmd,check=True)                
         elif cmd== "exit":
             break
         else:
              os.system(cmd)  
-             
+            
 if __name__ == '__main__':
     main()
+
